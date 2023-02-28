6d281c274a011bc1a20867a50072a8a0f11a70f9
Sebastien-Bardin <33619819+Sebastien-Bardin@users.noreply.github.com>
2023-02-28 13:24:46 +0100

    Fish Behaviour
    Fish are moving
    they can group
    They are going towards random position
    You can change distance between fishes in a group
    The destination is changing randomly you can adjust the frequency

--- Assets/Scripts/FishBehaviour.cs
+++ Assets/Scripts/FishBehaviour.cs
@@ -4,15 +4,103 @@ using UnityEngine;
 
 public class FishBehaviour : MonoBehaviour
 {
+    public float Speed = 5.0f;
+    public float ObstacleDistanceDetection = 1.0f;
+    public float RotationSpeed = 5.0f;
+    public float GroupingBehaviour = 5.0f;
+    public float GroupDist = 1.0f;
+    public float AligningBehaviour = 1.0f;
+
+    private List<GameObject> GroupMembers = new List<GameObject>();
+
+
+
+    private FishManager fishManager;
     // Start is called before the first frame update
+
+
+
     void Start()
     {
-        
+        fishManager = GetComponentInParent<FishManager>();
+        fishManager.FishSpeed = Speed;
+        fishManager.RotationSpeed = RotationSpeed;
+        fishManager.GroupingBehaviour = GroupingBehaviour;
+        fishManager.GroupDist = GroupDist;
+        fishManager.AligningBehaviour = AligningBehaviour;
+        fishManager.ObstacleDistanceDetection = ObstacleDistanceDetection;
+        GroupMembers = fishManager.FishList;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Speed = fishManager.FishSpeed;
+        RotationSpeed = fishManager.FishSpeed;
+        GroupingBehaviour = fishManager.GroupingBehaviour;
+        GroupDist = fishManager.GroupDist;
+        AligningBehaviour = fishManager.AligningBehaviour;
+        ObstacleDistanceDetection = fishManager.ObstacleDistanceDetection;
+
+
+        if (Random.Range(0.0f , 100.0f) < 25.0f){
+            ChangeDirection();
+            Debug.Log("ChangeDirection");
+        }
+
+
+        transform.Translate(0, 0, Speed * Time.deltaTime);
+
     }
+
+
+    private void ChangeDirection(){
+
+        
+
+        Vector3 grouping = Vector3.zero;
+        Vector3 following = Vector3.zero;
+        Vector3 avoiding = Vector3.zero;
+        
+
+        if (GroupMembers.Count > 0)
+        {
+            for (int i = 0; i < GroupMembers.Count; i++)
+            {
+                if (GroupMembers[i] != this.gameObject)
+                {
+                    //Grouping sum of fish pos
+                    grouping += GroupMembers[i].transform.position;
+
+                    //Avoiding members
+                    if (Vector3.Distance(GroupMembers[i].transform.position, this.transform.position) < GroupDist)
+                    {
+                        avoiding += (this.transform.position - GroupMembers[i].transform.position);
+                    }
+
+                }
+
+            }
+            //grouping point
+            grouping = (grouping / GroupMembers.Count) * GroupingBehaviour ;
+            Vector3 direction = (grouping  + avoiding + (fishManager.Destination-transform.position) ) - transform.position;
+            if (grouping != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction.normalized), RotationSpeed * Time.deltaTime);
+            }
+
+        }
+
+    }
+
+
+    /* #if(UNITY_EDITOR)
+         [ContextMenu("Initialize fish type")]
+         void IntitializeFish(){
+             fishManager = GetComponentInParent<FishManager>();
+             fishManager.FishSpeed = Speed;  
+         }
+
+     #endif*/
+
 }
--- Assets/Scripts/FishManager.cs
+++ Assets/Scripts/FishManager.cs
@@ -7,21 +7,52 @@ using UnityEngine;
 public class FishManager : MonoBehaviour
 {
 
-    public int FishNumber = 1;
-    public GameObject FishPrefab;
-    public Vector3 FishTankLimits = new Vector3(5, 5, 5);
     public List<GameObject> FishList = new List<GameObject>();
+
+    [Header("Group fish settings")]
+    public GameObject FishPrefab;
+    public int FishNumber = 1;
+    [Range(0.0f, 10.0f)]
+    public float FishSpeed;
+    [Range(0.0f, 10.0f)]
+    public float RotationSpeed;
+    [Range(0.0f, 1.0f)]
+    public float GroupingBehaviour;
+    [Range(0.0f, 10.0f)]
+    public float GroupDist;
+    [Range(0.0f, 10.0f)]
+    public float AligningBehaviour;
+    [Range(0.0f, 10.0f)]
+    public float ObstacleDistanceDetection;
+
+    [Range(0.0f, 100.0f)]
+    public float ChangingDestinationChance = 1.0f;
+
+    public float TimeBetweenDestChange = 2.0f;
+
+    public Vector3 GroupSpawnRange = new Vector3(5, 5, 5);
+
+    public Vector3 Destination = Vector3.zero;
+
+
+
+    private bool DestChanged =false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnFish();
+        Destination = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         SpawnFish();
+
+        ChangeDestination();
+        Debug.Log(DestChanged);
     }
 
 
@@ -37,8 +68,10 @@ public class FishManager : MonoBehaviour
             {
                 for (int i = FishList.Count; i < FishNumber; i++)
                 {
-                    Vector3 randomPos = transform.position + new Vector3(Random.Range(-FishTankLimits.x, FishTankLimits.x), Random.Range(-FishTankLimits.y, FishTankLimits.y), Random.Range(-FishTankLimits.z, FishTankLimits.z));
-                    FishList.Add(Instantiate(FishPrefab, randomPos, Quaternion.identity));
+                    Vector3 randomPos = transform.position + new Vector3(Random.Range(-GroupSpawnRange.x, GroupSpawnRange.x), Random.Range(-GroupSpawnRange.y, GroupSpawnRange.y), Random.Range(-GroupSpawnRange.z, GroupSpawnRange.z));
+                    GameObject fish = Instantiate(FishPrefab, randomPos, Quaternion.identity);
+                    fish.transform.parent = gameObject.transform;
+                    FishList.Add(fish);
 
                 }
             }
@@ -53,4 +86,25 @@ public class FishManager : MonoBehaviour
             }
         }
     }
+
+    IEnumerator ChangeDestinationTimer(){
+
+        yield return new WaitForSeconds(TimeBetweenDestChange);
+        DestChanged =false;
+        
+    }
+
+    private void ChangeDestination(){
+        
+        if (Random.Range(0,100)<ChangingDestinationChance && !DestChanged)
+        {
+            Destination = transform.position + new Vector3(Random.Range(-GroupSpawnRange.x, GroupSpawnRange.x), Random.Range(-GroupSpawnRange.y, GroupSpawnRange.y), Random.Range(-GroupSpawnRange.z, GroupSpawnRange.z));
+            DestChanged = true;
+            StartCoroutine(ChangeDestinationTimer());
+           
+        }
+    }
+
+
+    
 }
